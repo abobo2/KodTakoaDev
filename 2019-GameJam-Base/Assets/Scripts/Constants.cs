e82b8a16f0074511a2e15d07747a46287cbe585b
Ace <kellerr@abv.bg>
2019-01-25 22:16:52 +0200

    Add basic bitch character controller

---
+++ 2019-GameJam-Base/Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public static class Constants
+{
+    public static Func<Vector2, Vector3> InputToMotionFunction => (v2) =>
+        {
+            Quaternion targetRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+            return targetRotation * new Vector3(v2.x, 0, v2.y);
+        };
+}
---
+++ 2019-GameJam-Base/Assets/Scripts/Controller/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public Quaternion targetRotation;
+
+    public Vector2 playerInput;
+
+    public float MoveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+        Rotate();
+        
+    }
+
+    void GetInput()
+    {
+        playerInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    void Rotate()
+    { 
+        Debug.Log("TODO");
+        transform.LookAt(transform.position + Constants.InputToMotionFunction(playerInput) );
+//        transform.rotate;
+//        Mathf.Atan2()
+      
+    }
+
+    void Move()
+    {
+        
+        transform.Translate(Constants.InputToMotionFunction(playerInput) * MoveSpeed, Space.World);
+    }
+}
