e38d9685caebbfcd6d5cf202c7d2ae509d0c7cee
Ace <kellerr@abv.bg>
2019-01-26 12:43:07 +0200

    Add Extra function

--- 2019-GameJam-Base/Assets/Scripts/Constants.cs
+++ 2019-GameJam-Base/Assets/Scripts/Constants.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 public static class Constants
 {
     public static Func<Vector2, Vector3> InputToMotionFunction => (v2) =>
-        {
-            Quaternion targetRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
-            return targetRotation * new Vector3(v2.x, 0, v2.y);
-        };
+    {
+        Quaternion targetRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        return targetRotation * new Vector3(v2.x, 0, v2.y);
+    };
+
+    public static Func<Vector3, Vector3> CameraSpaceFunction => (v3) =>
+    {
+        Quaternion targetRotation = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up);
+        return targetRotation * v3;
+    };
+
+    public static string PlayerTag => "Player";
 }
