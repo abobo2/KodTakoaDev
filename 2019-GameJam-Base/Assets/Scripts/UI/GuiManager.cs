2df2d75270c3fd587e0c9eb40aded03be3912cfe
hpetrov <h.petrov@xs-software.com>
2019-01-26 14:34:14 +0200

    level timer

--- 2019-GameJam-Base/Assets/Scripts/UI/GuiManager.cs
+++ 2019-GameJam-Base/Assets/Scripts/UI/GuiManager.cs
@@ -14,22 +14,32 @@ public class GuiManager : MonoBehaviour, IInitiatable
     public TasksListManager tasksManager;
 
     private GameState data;
-
+    
     public void Initiate()
     {
         data = ServiceLocator.instance.GetInstanceOfType<GameState>();
 
-        data.energy.Subscribe(newVal => OnEnergyVhange(newVal));
+        data.energy.Subscribe(newVal => OnEnergyChange(newVal));
         data.timer.Subscribe(newTime => OnTimerChanged(newTime));
     }
 
-    private void OnEnergyVhange(int newEnergyVal)
+    private void OnEnergyChange(int newEnergyVal)
     {
 
     }
 
     private void OnTimerChanged(int seconds)
     {
+        if (seconds >= 0)
+        {
+            txtTimer.gameObject.SetActive(true);
 
+            TimeSpan span = TimeSpan.FromSeconds(seconds);
+            txtTimer.text = span.Minutes.ToString().PadLeft(2, '0') + ":" + span.Seconds.ToString().PadLeft(2, '0');
+        }
+        else
+        {
+            txtTimer.gameObject.SetActive(false);
+        }
     }
 }
